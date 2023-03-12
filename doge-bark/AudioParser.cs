972948d7eee2605a9bcb1b7fec8733b68ae2bd82
Otto Piskonen <otto.piskonen@gmail.com>
2023-03-12 12:16:10 +0200

    working commit

---
+++ doge-bark/AudioParser.cs
@@ -0,0 +1,120 @@
+﻿using NAudio.Wave;
+
+namespace doge_bark
+{
+    public class AudioParser
+    {
+        private readonly int threshold;
+        private readonly int clipLength;
+        private readonly string outputPath;
+
+        private WaveFileWriter? writer = null;
+        private bool stopRecording = false; // flag for stopping recording
+        private bool recordingInProgress = false; // flag to indicate recording state
+
+        public AudioParser(string outputPath, int threshold, int clipLength)
+        {
+            this.outputPath = Path.Combine(outputPath, "Clips");
+            this.threshold = threshold;
+            this.clipLength = clipLength;
+        }
+
+        /// <summary>
+        /// Main loop for the program
+        /// </summary>
+        public void StartRecording()
+        {
+            // create the output path to the Clips
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            WaveInEvent waveIn = new()
+            {
+                WaveFormat = new WaveFormat(44100, 16, 1)
+            };
+
+            waveIn.DataAvailable += WaveInDataAvailable;
+            waveIn.StartRecording();
+
+            Console.WriteLine("Recording... Press any key to stop.");
+            Console.ReadKey();
+
+            // set the flag to stop recording
+            stopRecording = true;
+
+            waveIn.StopRecording();
+            waveIn.Dispose();
+        }
+
+        /// <summary>
+        /// EventHandler to check if we exceed the threshold and then write clipLength durations to a clip
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WaveInDataAvailable(object sender, WaveInEventArgs e)
+        {
+            // check if recording should stop
+            if (stopRecording)
+            {
+                // close the file and reset the writer
+                if (writer != null)
+                {
+                    writer.Dispose();
+                    writer = null;
+                }
+                return;
+            }
+
+            // check if the RMS value exceeds the threshold
+            if (recordingInProgress)
+            {
+                WriteAudio(e);
+            }
+            else
+            {
+                // calculate the RMS value of the audio data
+                double rms = 0;
+                for (int i = 0; i < e.BytesRecorded; i += 2)
+                {
+                    short sample = (short)((e.Buffer[i + 1] << 8) | e.Buffer[i]);
+                    rms += Math.Pow(sample, 2);
+                }
+                rms = Math.Sqrt(rms / (e.BytesRecorded / 2));
+
+                // check if the RMS value exceeds the threshold
+                if (rms > threshold)
+                {
+                    recordingInProgress = true;
+                    Console.WriteLine($"Threshold exceeded. Saving a clip..");
+                    if (writer == null)
+                    {
+                        string filename = Path.Combine(outputPath, $"clip{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.wav");
+                        writer = new WaveFileWriter(filename, new WaveFormat(44100, 16, 1));
+                    }
+                    WriteAudio(e);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Write event buffer to audio and check if we have exceeded the clipLength
+        /// </summary>
+        /// <param name="e">Audio event args</param>
+        public void WriteAudio(WaveInEventArgs e)
+        {
+            // write the audio data to the file
+            writer.Write(e.Buffer, 0, e.BytesRecorded);
+
+            // check if the recording has exceeded the clip length
+            if (writer.Length > clipLength * writer.WaveFormat.AverageBytesPerSecond)
+            {
+                // close the file and reset the writer
+                writer.Dispose();
+                writer = null;
+                recordingInProgress = false;
+            }
+        }
+    }
+}
--- doge-bark/Program.cs
+++ doge-bark/Program.cs
@@ -1,109 +1,94 @@
-﻿using NAudio.Wave;
+﻿using doge_bark;
 
-Console.WriteLine("Give output folder:");
-var outputPath = Console.ReadLine();
-
-AudioParser audioParser = new();
-audioParser.StartRecording(outputPath);
-
-public class AudioParser
+// Starting animation - skip with any key
+Console.Write("Welcome to doge-bark");
+for (int i = 0; i < 10; i++)
 {
-    private readonly int threshold;
-    private readonly int clipLength;
-    private readonly string outputPath;
-
-    private WaveFileWriter writer = null;
-    private List<short> audioBuffer = new();
-    private bool stopRecording = false; // flag for stopping recording
-
-    public AudioParser() { }
-    public AudioParser(string outputPath, int threshold, int clipLength)
+    if (Console.KeyAvailable)
     {
-        this.threshold = threshold;
-        this.clipLength = clipLength;
-        this.outputPath = outputPath;
+        Console.ReadKey(true);
+        break;
     }
+    Console.Write(".");
+    Thread.Sleep(300);
 
-    private void WaveInDataAvailable(object sender, WaveInEventArgs e)
+    if ((i + 1) % 3 == 0)
     {
-        string outputPath = Path.Combine(this.outputPath, "Clips");
-        if (!Directory.Exists(outputPath))
+        Console.Write("\b\b\b   \b\b\b");
+        Thread.Sleep(300);
+    }
+}
+Console.WriteLine();
+
+// Options for the user
+int threshold = 3000;
+int clipLength = 25;
+int selection = 0;
+while (selection != 1)
+{
+    Console.WriteLine("Please select an option:");
+    Console.WriteLine("1 - Run the program");
+    Console.WriteLine("2 - Set Options");
+    Console.WriteLine($"Current settings: clip length {clipLength} second | threshold {threshold} RMS");
+    ConsoleKeyInfo userInput = Console.ReadKey();
+
+    if (int.TryParse(userInput.KeyChar.ToString(), out selection))
+    {
+        Console.WriteLine();
+        switch (selection)
         {
-            Directory.CreateDirectory(outputPath);
-        }
+            case 1:
+                Console.WriteLine("Give output folder:");
+                var outputPath = Console.ReadLine();
+                // If output path is not given use the directory of .exe
+                outputPath ??= Directory.GetCurrentDirectory();
 
-        // check if recording should stop
-        if (stopRecording)
-        {
-            // close the file and reset the writer
-            if (writer != null)
-            {
-                writer.Dispose();
-                writer = null;
-            }
-            return;
-        }
+                AudioParser audioParser = new(outputPath, threshold, clipLength);
+                Console.Clear();
+                audioParser.StartRecording();
+                break;
+            case 2:
+                // Read clipLength from user
+                Console.WriteLine("Give clip length in seconds:");
+                string userClipLengthInput = Console.ReadLine();
+                if(int.TryParse(userClipLengthInput, out int userClipLength))
+                {
+                    clipLength = userClipLength;
+                }
 
-        // calculate the RMS value of the audio data
-        double rms = 0;
-        for (int i = 0; i < e.BytesRecorded; i += 2)
-        {
-            short sample = (short)((e.Buffer[i + 1] << 8) | e.Buffer[i]);
-            rms += Math.Pow(sample, 2);
-        }
-        rms = Math.Sqrt(rms / (e.BytesRecorded / 2));
-
-        // add the audio data to the buffer
-        for (int i = 0; i < e.BytesRecorded; i += 2)
-        {
-            short sample = (short)((e.Buffer[i + 1] << 8) | e.Buffer[i]);
-            audioBuffer.Add(sample);
-        }
-
-        // check if the RMS value exceeds the threshold
-        if (rms > threshold)
-        {
-            // start a new recording if necessary
-            if (writer == null)
-            {
-                string filename = Path.Combine(outputPath, $"clip{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.wav");
-                writer = new WaveFileWriter(filename, new WaveFormat(44100, 16, 1));
-            }
-
-            // write the audio data to the file
-            writer.Write(e.Buffer, 0, e.BytesRecorded);
-
-            // check if the recording has exceeded the clip length
-            if (writer.Length > clipLength * writer.WaveFormat.AverageBytesPerSecond)
-            {
-                // close the file and reset the writer
-                writer.Dispose();
-                writer = null;
-            }
+                // Read threshold from user
+                Console.WriteLine("Give threshold in RMS <1000 is normal speaking voice>:");
+                string userThresholdInput = Console.ReadLine();
+                if(int.TryParse(userThresholdInput, out int userThreshold))
+                {
+                    threshold = userThreshold;
+                }
+                Console.Clear();
+                break;
+            default:
+                Console.WriteLine("Invalid option");
+                break;
         }
     }
+    else Console.WriteLine("Invalid option");
+}
 
-    public void StartRecording(string outputPath, int threshold = 10000, int clipLength = 25)
+// Closing animation
+Console.Clear();
+Console.Write("Program shutting down");
+for (int i = 0; i < 10; i++)
+{
+    if (Console.KeyAvailable)
     {
-        WaveInEvent waveIn = new()
-        {
-            WaveFormat = new WaveFormat(44100, 16, 1)
-        };
+        Console.ReadKey(true);
+        break;
+    }
+    Console.Write(".");
+    Thread.Sleep(300);
 
-        waveIn.DataAvailable += new EventHandler<WaveInEventArgs>((sender, e) =>
-        {
-            AudioParser audioParser = new AudioParser(outputPath, threshold, clipLength);
-            audioParser.WaveInDataAvailable(sender, e);
-        });
-        waveIn.StartRecording();
-
-        Console.WriteLine("Recording... Press any key to stop.");
-        Console.ReadKey();
-
-        // set the flag to stop recording
-        stopRecording = true;
-
-        waveIn.StopRecording();
-        waveIn.Dispose();
+    if ((i + 1) % 3 == 0)
+    {
+        Console.Write("\b\b\b   \b\b\b");
+        Thread.Sleep(300);
     }
 }
